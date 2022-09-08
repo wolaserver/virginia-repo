9f34504140c673d8300988dff654cb182500de1b
Wola Publicidad <108073209+wolaserver@users.noreply.github.com>
2022-09-08 09:52:16 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
-          <RiHeart2Line />
-        </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        Copyright © Todos los derechos reservados. - Wola Publicidad
       </p>
     </div>
   </footer>
